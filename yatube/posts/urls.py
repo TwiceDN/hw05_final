cff0992df136ad7a0c3957e010354b558e1af9e1
TwiceDN <106080846+TwiceDN@users.noreply.github.com>
2022-10-16 18:42:36 +0300

    hw5.9

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,50 +6,53 @@ app_name = 'posts'
 
 
 urlpatterns = [
-    path(
+     path(
           '',
           views.index,
           name='index'
      ),
-    path(
+     path(
           'group/<slug:slug>/',
           views.group_posts,
           name="group_list"
      ),
-    path(
+     path(
           'profile/<str:username>/',
           views.profile,
           name='profile'
      ),
-    path(
+     path(
           'posts/<int:post_id>/',
           views.post_detail,
           name='post_detail'
      ),
-    path(
+     path(
           'create/',
           views.post_create,
           name='create'
      ),
-    path(
+     path(
           'posts/<int:post_id>/edit/',
           views.post_edit,
           name='edit'
      ),
-    path(
+     path(
           'posts/<int:post_id>/comment/',
           views.add_comment,
           name='add_comment'
      ),
-    path('follow/',
-         views.follow_index,
-         name='follow_index'),
+     path(
+          'follow/',
+          views.follow_index,
+          name='follow_index'),
 
-    path('profile/<str:username>/follow/',
-         views.profile_follow,
-         name='profile_follow'),
+     path(
+          'profile/<str:username>/follow/',
+          views.profile_follow,
+          name='profile_follow'),
 
-    path('profile/<str:username>/unfollow/',
-         views.profile_unfollow,
-         name='profile_unfollow'),
+     path(
+          'profile/<str:username>/unfollow/',
+          views.profile_unfollow,
+          name='profile_unfollow'),
 ]
