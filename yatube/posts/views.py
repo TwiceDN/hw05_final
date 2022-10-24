ae0088109f3d2e87ab0df06c94615cbaf30c4d31
DanilaN <YakovlevDanila.N@yandex.ru>
2022-10-24 15:46:02 +0300

    Change requirements

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,8 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.cache import cache_page
 
-from .models import Comment, Follow, Group, Post, User
 from .forms import CommentForm, PostForm
+from .models import Comment, Follow, Group, Post, User
 from .utils import paginator_def
 
 
